18d51235983fb0ffae91ab8601e37222576babb8
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2023-01-14 10:55:23 +0900

    update yolo_train_test.py

--- test/yolo_train_test.py
+++ test/yolo_train_test.py
@@ -1,21 +1,24 @@
-from yolov5 import train 
+from ultralytics import YOLO
 from argparse import ArgumentParser
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('--epochs', type = int, default = 4, help='total training epochs')
     parser.add_argument('--batch', type=int, default=16, help='total batch size')
-    parser.add_argument('--weights', type = str, default = 'yolov5s.pt', help = 'initial weights path')
+    parser.add_argument('--weights', type = str, default = 'yolov8n.pt', help = 'initial weights path')
     args = parser.parse_args()
 
     epochs = args.epochs
     batch_size = args.batch
     weights = args.weights
 
-    train.run(data='train.yaml', 
-    batch_size = batch_size,
-    epochs = epochs,
-    weights = weights)
+
+    model = YOLO(weights)
+    model.train(
+        data = 'train.yaml',
+        epochs = epochs,
+        batch = batch_size,
+    )
 
 if __name__ == '__main__':
     main()
